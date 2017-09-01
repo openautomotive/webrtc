163fa36e1c68b151a0b1690538e63739e220224f
Sam Zackrisson <saza@webrtc.org>
2017-09-01 14:44:39 +0200

    Lift optional APM construction to the Java layer.
    This adds a Java class AudioProcessing, wrapping its C++ namesake.
    This also extends the PeerConnectionFactory to accept an
    AudioProcessing instance in its constructor, which encompasses the bulk
    of the work.
    
    Bug: webrtc:8163
    Change-Id: Iea2b5681a76d1281ece2ef15d9956a95cbb038a9

---
+++ webrtc/sdk/android/instrumentationtests/src/org/webrtc/AudioProcessingTest.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package org.webrtc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.MediumTest;
+import android.support.test.filters.SmallTest;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Unit tests for {@link AudioProcessing}.
+ * Expected to be grow as the APM API is lifted to the Java layer.
+ */
+@RunWith(BaseJUnit4ClassRunner.class)
+public final class AudioProcessingTest {
+  @Before
+  public void setUp() {
+    PeerConnectionFactory.initializeAndroidGlobals(InstrumentationRegistry.getContext(), false);
+  }
+
+  @Test
+  @SmallTest
+  public void testCreateDelete() {
+    AudioProcessing apm = new AudioProcessing();
+    apm.release();
+  }
+
+  @Test
+  @MediumTest
+  public void testInitializePeerConnectionFactory() {
+    AudioProcessing apm = new AudioProcessing();
+    PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+    PeerConnectionFactory factory = new PeerConnectionFactory(options, null, null, apm);
+    factory.dispose();
+  }
+}
