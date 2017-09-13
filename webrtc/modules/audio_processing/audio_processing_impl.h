df604982d00087a6d541a79d4b08e236bbaedc22
Sam Zackrisson <saza@webrtc.org>
2017-09-13 10:55:40 +0200

    git cl format
    fix small things and add unit test
    
    
    Change type of ToString().
    
    
    Polish PostProcessing init
    
    
    Move post processing module interface definition into APM header.
    
    
    Fix bugs.
    
    
    Add log statement for post processor activation.
    
    
    Add postprocessor in APM implementaiton.
    
    
    Add PostProcessing injection to APM.
    
    
    Add PostProcessing interface header.
    
    Bug:
    Change-Id: I584002b3a77b151cbf474c7efd5187ccb9eb7474

--- webrtc/modules/audio_processing/audio_processing_impl.h
+++ webrtc/modules/audio_processing/audio_processing_impl.h
@@ -39,8 +39,10 @@ class AudioProcessingImpl : public AudioProcessing {
   // Methods forcing APM to run in a single-threaded manner.
   // Acquires both the render and capture locks.
   explicit AudioProcessingImpl(const webrtc::Config& config);
-  // AudioProcessingImpl takes ownership of beamformer.
+  // AudioProcessingImpl takes ownership of capture post processor and
+  // beamformer.
   AudioProcessingImpl(const webrtc::Config& config,
+                      std::unique_ptr<PostProcessing> capture_post_processor,
                       NonlinearBeamformer* beamformer);
   ~AudioProcessingImpl() override;
   int Initialize() override;
@@ -156,7 +158,8 @@ class AudioProcessingImpl : public AudioProcessing {
                 bool echo_canceller3_enabled,
                 bool voice_activity_detector_enabled,
                 bool level_estimator_enabled,
-                bool transient_suppressor_enabled);
+                bool transient_suppressor_enabled,
+                bool capture_post_processor_enabled);
     bool CaptureMultiBandSubModulesActive() const;
     bool CaptureMultiBandProcessingActive() const;
     bool CaptureFullBandProcessingActive() const;
@@ -178,6 +181,7 @@ class AudioProcessingImpl : public AudioProcessing {
     bool level_estimator_enabled_ = false;
     bool voice_activity_detector_enabled_ = false;
     bool transient_suppressor_enabled_ = false;
+    bool capture_post_processor_enabled_ = false;
     bool first_update_ = true;
   };
 
@@ -218,7 +222,8 @@ class AudioProcessingImpl : public AudioProcessing {
       RTC_EXCLUSIVE_LOCKS_REQUIRED(crit_render_, crit_capture_);
   void InitializeLowCutFilter() RTC_EXCLUSIVE_LOCKS_REQUIRED(crit_capture_);
   void InitializeEchoCanceller3() RTC_EXCLUSIVE_LOCKS_REQUIRED(crit_capture_);
-  void InitializeGainController2();
+  void InitializeGainController2() RTC_EXCLUSIVE_LOCKS_REQUIRED(crit_capture_);
+  void InitializePostProcessor() RTC_EXCLUSIVE_LOCKS_REQUIRED(crit_capture_);
 
   void EmptyQueuedRenderAudio();
   void AllocateRenderQueue()
@@ -367,6 +372,7 @@ class AudioProcessingImpl : public AudioProcessing {
     bool level_controller_enabled = false;
     bool echo_canceller3_enabled = false;
     bool gain_controller2_enabled = false;
+    bool capture_post_processor_enabled = false;
   } capture_nonlocked_;
 
   struct ApmRenderState {
